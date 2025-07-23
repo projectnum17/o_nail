63fa381490780b1f41d21864352e5b2c1a28df36
projectnum17 <egor.chornyy@gmail.com>
2025-07-23 10:03:37 +0300

    layout register flow

--- assets/scripts/modules/form.js
+++ assets/scripts/modules/form.js
@@ -4,8 +4,27 @@ const form = () => {
     if (!forms.length) return;
 
     forms.forEach((form) => {
+        const phoneInput = form.querySelector('input[type="tel"]');
+        if (phoneInput) {
+            phoneInput.addEventListener('input', function (e) {
+                const value = e.target.value.replace(/\D/g, '');
+                const match = value.match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+
+                if (match) {
+                    e.target.value = !match[2]
+                        ? match[1]
+                        : '(' +
+                          match[1] +
+                          ') ' +
+                          match[2] +
+                          (match[3] ? '-' + match[3] : '');
+                }
+            });
+        }
+
         form.addEventListener('submit', (e) => {
             e.preventDefault();
+            form.reset();
         });
     });
 };
